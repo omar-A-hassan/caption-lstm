74494dc1750da54b268500e7f816484bab1984cf
Omar-A-hassan <quickyomar@gmail.com>
2025-11-02 13:03:55 +0200

    Fix __init__.py: import FiLMGenerator instead of SimpleFusion

--- caption_lstm/__init__.py
+++ caption_lstm/__init__.py
@@ -1,6 +1,6 @@
 from caption_lstm.model import ViLCap, ViLCapConfig
 from caption_lstm.decoder import CaptionDecoder, CaptionDecoderConfig
-from caption_lstm.fusion import SimpleFusion
+from caption_lstm.fusion import FiLMGenerator
 from caption_lstm.tokenizer import CaptionTokenizer
 from caption_lstm.mbr_decoder import MBRCaptionDecoder
 
@@ -9,7 +9,7 @@ __all__ = [
     "ViLCapConfig",
     "CaptionDecoder",
     "CaptionDecoderConfig",
-    "SimpleFusion",
+    "FiLMGenerator",
     "CaptionTokenizer",
     "MBRCaptionDecoder",
 ]
